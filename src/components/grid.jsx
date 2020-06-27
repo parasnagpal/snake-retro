e32050de56273ba89e01cac7bdd78a5d67b95ab7
Paras Nagpal <paras.nag@gmail.com>
2020-06-27 05:41:22 +0530

    snake movement

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -6,29 +6,93 @@ class Grid extends React.Component{
         super(props);
         this.state={
             rows:0,
-            cols:0
+            cols:0,
+            snakeGrid:[],
+            snakeCoordinates:[[0,0]],
+            dir:'R'
         }
+    }
+
+    prepareSnakeState(){
+        let snakeGrid=[[0]];
+        let rows=this.state.rows;
+        let cols=this.state.cols;
+        let snakeCoordinates=this.state.snakeCoordinates;
+
+        for(let i=0;i<rows;i++){
+            let newRow=[]
+            for(let j=0;j<cols;j++){
+                newRow.push(0);
+            }
+            snakeGrid.push(newRow);
+        }
+
+        for(let i=0;i<snakeCoordinates.length;i++){
+            let u=snakeCoordinates[i][0],v=snakeCoordinates[i][1];
+            snakeGrid[u][v]=1;
+        }
+        
+        this.setState({
+            snakeGrid:snakeGrid,
+            snakeCoordinates:snakeCoordinates
+        })
+    }
+
+    snakeMovement(){
+        var snakeGrid=this.state.snakeGrid;
+        var snakeCoordinates=this.state.snakeCoordinates;
+        setInterval(()=>{
+            let firstx=snakeCoordinates[0][0];
+            let firsty=snakeCoordinates[0][1];
+            let lastx=snakeCoordinates[snakeCoordinates.length-1][0];
+            let lasty=snakeCoordinates[snakeCoordinates.length-1][1];
+            //console.log(firstx+1)
+            snakeCoordinates.push([firstx+1,firsty]);
+            snakeCoordinates.shift();
+            snakeGrid[firstx+1][firsty]=1;
+            snakeGrid[lastx][lasty]=0;
+            console.log(snakeCoordinates)
+        },1000);
     }
 
     componentDidMount(){
         this.setState({
-            rows:this.props.height-5,
-            cols:this.props.width-5
+            rows:this.props.height-4,
+            cols:this.props.width-4
+        },()=>{
+            let p=new Promise((resolve,reject)=>{
+                this.prepareSnakeState();
+                resolve();
+            })
+            p.then(()=>{
+                this.snakeMovement();
+            })
         })
     }
 
-    addcols(){
+    addcols(row){
         let columns=[];
         const style=StyleSheet.create({
             pixel:{
                 borderColor:"grey",
-                borderWidth:0.5,
+                borderWidth:1,
                 height:10,
                 width:10,
+            },
+            snake:{
+                borderColor:"grey",
+                borderWidth:1,
+                height:10,
+                width:10,
+                backgroundColor:"black"
             }
         })
-        for(let i=0;i<this.state.cols;i++)
-            columns.push(<View key={i} style={style.pixel}></View>);
+        for(let i=0;i<this.state.cols;i++){
+            if(row<this.state.snakeGrid.length && this.state.snakeGrid[row][i])
+                columns.push(<View key={i} style={style.snake}></View>);
+            else    
+                columns.push(<View key={i} style={style.pixel}></View>);
+        }
         return columns;    
     }
 
@@ -40,7 +104,7 @@ class Grid extends React.Component{
             }
         })
         for(let i=0;i<this.state.rows;i++)
-            grid_layout.push(<View key={i} style={style.row}>{this.addcols()}</View>);
+            grid_layout.push(<View key={i} style={style.row}>{this.addcols(i)}</View>);
         return grid_layout;
     }
 
