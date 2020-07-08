dd2b757b948579a18935332c83460184525bbd73
Paras Nagpal <paras.nag@gmail.com>
2020-07-08 06:36:51 +0530

    Screens

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import GestureView from './src/components/gesture'
+import GestureView from './src/components/gesture';
+import 'react-native-gesture-handler';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <GestureView/> 
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -25,3 +22,21 @@ const styles = StyleSheet.create({
     flex:1
   }
 });
+
+function Arena(){
+  return(
+    <View style={styles.container}>
+      <GestureView/> 
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Arena" component={Arena}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
